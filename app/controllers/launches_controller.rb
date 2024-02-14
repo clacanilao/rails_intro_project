bebde2319e6ad7d726a111e18c05ae8a11ac4600
Clarence Lacanilao <clacanilao@academic.rrc.ca>
2024-02-14 15:46:10 -0600

    Added Member Page Layouts and Mapping

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -5,6 +5,10 @@ class LaunchesController < ApplicationController
 
   def show
     @launch = Launch.find(params[:id])
+    @rocketname = Rocket.where(id: @launch.rocket_id)
+                        .pluck(:name)
+    @launchpadname = Launchpad.where(id: @launch.launchpad_id)
+                        .pluck(:name)
     @crews = Crew.joins(assignments: :launch)
                  .where("launches.id = ?", params[:id])
                  .pluck("crews.id", "crews.name", "assignments.role")
